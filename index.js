d4398d8947af00487229a421f84e82a8dcfc28d3
Dana Shacham <dshacham22@gmail.com>
2020-02-04 13:25:14 +0100

    add js and fontawesome

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 (() => {
+    const h2 = document.querySelector('h2');
+    const button = document.querySelector('button');
 
+    const randomQuote = function () {
+        const quotes = ['But I don\'t wanna be a pirate!', 'Finally my stupidity pays off!', 'Guiddy up', 'They\'re real, and they\'re fabulous.', 'No soup for you!'];
+
+        const generateQuote = quotes[Math.floor(quotes.length * Math.random())];
+
+        return h2.textContent = generateQuote;
+    }
+
+    window.addEventListener('load', randomQuote, false);
+    button.addEventListener('click', randomQuote, false);
 })();
